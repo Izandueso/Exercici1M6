deb641c76eef88ded1471ce412d0fe6d6ef7b61a
Alumne <Alumne@104-PC12>
2020-01-09 16:40:22 +0100

    SegonCommit

--- src/Exercici1M6.java
+++ src/Exercici1M6.java
@@ -8,20 +8,18 @@ public class Exercici1M6 {
 		 
 		 System.out.println("Introdueix el directori o fitxer que vols consultar:");
 		 String fichero = teclado.nextLine();
-		 File fitxer = new File(fichero);
-		 Scanner scan = new Scanner(fitxer);
+		 File f = new File(fichero);
+		 Scanner scan = new Scanner(f);
 		 
-		 if (fitxer.exists()){
-			 if(fitxer.isDirectory()){
+		 if (f.exists()){
+			 if(f.isDirectory()){
 				 System.out.println("Fitxers al directori actual: ");
-				 File f = new File(".");
 				 String[] arxius = f.list();
 				 for (int i = 0; i<arxius.length; i++){
 					 System.out.println(arxius[i]);
 				 }
-			 }else if(fitxer.isFile()){
+			 }else if(f.isFile()){
 				 System.out.println("INFORMACIÓ SOBRE EL FITXER");
-				 File f = new File("javi");
 				 if(f.exists()){
 					 System.out.println("Nom del fitxer : "+f.getName());
 					 System.out.println("Ruta           : "+f.getPath());
